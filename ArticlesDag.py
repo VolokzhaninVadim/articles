1abe8c6add7e341d356e31e23d099099a597454d
Volokzhanin Vadim <volokzhanin@yandex.ru>
2021-08-29 13:53:05 +1000

    article_scraper set parameters, habr_bookmarks

--- ArticlesDag.py
+++ ArticlesDag.py
@@ -18,13 +18,27 @@ pg_password = os.environ['PG_PASSWORD']
 pg_login = os.environ['LOGIN_NAME']
 pg_host = os.environ['PG_HOST']
 password_tor = os.environ['PASSWORD_TOR']
+yandex_mail = os.environ['YANDEX_MAIL']
+habr_password = os.environ['HABR_PASSWORD']
 
 # Создаем функцию для получения новых данных с Habr
-def article_scraper():
+def article_scraper(
+    pg_password = pg_password
+    ,pg_login = pg_login
+    ,host = pg_host
+    ,password_tor = password_tor
+    ,yandex_mail = yandex_mail
+    ,habr_password = habr_password
+    ):
     """
     Получаем новые данные с Habr.
     Вход: 
-        нет.
+        pg_password(str) - пароль к pg.
+        pg_login(str) - логин к pg.
+        host(str) - хост pg.
+        password_tor(str) - пароль к tor.
+        yandex_mail(str) - yandex почта.
+        habr_password(str) - пароль к habr.
     Выход: 
         нет.
     """
@@ -33,6 +47,8 @@ def article_scraper():
         ,pg_login = pg_login
         ,host = pg_host
         ,password_tor = password_tor
+        ,yandex_mail = yandex_mail
+        ,habr_password = habr_password
     )
     return article_scraper
 
@@ -60,9 +76,16 @@ with DAG(
 # Получаем статьи с Habr
     all_new_habr = PythonOperator(
         task_id = "habr_loader", 
-        python_callable = all_new_habr().all_new, 
+        python_callable = article_scraper().all_new, 
+        dag = dag
+        )
+    
+# Получаем закладки и проставляем отметку о закладке
+    habr_bookmarks = PythonOperator(
+        task_id = "habr_bookmarks", 
+        python_callable = article_scraper().mark_bookmarks, 
         dag = dag
         ) 
 
 # Порядок выполнения задач
-all_new_habr  
+    all_new_habr >> habr_bookmarks   
